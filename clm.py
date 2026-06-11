5f6afba560e2870d140ab5c2b4baa3ba2d8b0c03
MVVSKrishnaChaitanya <160884091+MVVSKrishnaChaitanya@users.noreply.github.com>
2026-06-11 15:46:03 +0530

    Update status is addedclm.py

--- clm.py
+++ clm.py
@@ -1,4 +1,10 @@
 valid_status = ["Draft", "Pending Approval", "Approved", "Signed"]
+def move_to_approval(name):
+    for c in contracts:
+        if c["name"] == name and c["status"] == "Draft":
+            c["status"] = "Pending Approval"
+            print("✅ Moved to Approval Stage")
+            return
 
 def update_status(name, new_status):
     if new_status not in valid_status:
